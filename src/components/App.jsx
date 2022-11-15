8a8a749462423ca6d3192bb6b1fb12b23a6ef937
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-11-15 13:12:57 +0300

    Initialization and Setup, files structurs

---
+++ src/components/App.jsx
@@ -0,0 +1,26 @@
+import { CssBaseline } from '@mui/material';
+import { Route, Routes } from 'react-router-dom';
+
+import { Content, Main, StyledToolbar } from './styles';
+
+import { Movies, MovieInformation, Actors, Profile, Navbar } from './';
+
+const App = () => {
+  return (
+    <Main>
+      <CssBaseline />
+      <Navbar />
+      <Content>
+        <StyledToolbar />
+        <Routes>
+          <Route path="/" element={<Movies />} />
+          <Route path="/movie/:id" element={<MovieInformation />} />
+          <Route path="/actors/:id" element={<Actors />} />
+          <Route path="/profile/:id" element={<Profile />} />
+        </Routes>
+      </Content>
+    </Main>
+  );
+};
+
+export default App;
---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+export { default as Actors } from './actors/Actors';
+export { default as MovieInformation } from './movie-information/MovieInformation';
+export { default as Movies } from './movies/Movies';
+export { default as Navbar } from './navbar/Navbar';
+export { default as Profile } from './profile/Profile';
