43d2eb34a3921d0400fa84dc52c53ff5790d9baa
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-11-21 12:24:31 +0300

    Redux Setup, fetch data from themoviedb.org

--- src/components/movies/Movies.jsx
+++ src/components/movies/Movies.jsx
@@ -1,4 +1,17 @@
+import { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import {
+  Box,
+  CircularProgress,
+  useMediaQuery,
+  Typography,
+} from '@mui/material';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
+
 const Movies = () => {
+  const { data } = useGetMoviesQuery();
+  console.log(data);
   return <div>Movies</div>;
 };
 
