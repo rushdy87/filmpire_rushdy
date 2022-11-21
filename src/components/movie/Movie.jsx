e94f7ca0d392b2170d77e6fd01cb3429c1f2949f
“rushdy87” <48268026+m7md7sam@users.noreply.github.com>
2022-11-21 23:26:12 +0300

    Finishing Movies View

--- src/components/movie/Movie.jsx
+++ src/components/movie/Movie.jsx
@@ -6,11 +6,31 @@ const Movie = ({ movie, index }) => {
   const navigate = useNavigate();
   return (
     <Grid item xs={12} sm={6} md={4} lg={3} xl={2} p={'10px'}>
-      <Links onClick={() => navigate(`/movie/${movie.id}`)}>
-        <Title className="title" variant="h5">
-          {movie.title}
-        </Title>
-      </Links>
+      <Grow in key={index} timeout={(index + 1) * 250}>
+        <Links onClick={() => navigate(`/movie/${movie.id}`)}>
+          <Box sx={{ overflow: 'hidden', borderRadius: '5px', height: 300 }}>
+            <Img
+              src={
+                movie.poster_path
+                  ? `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
+                  : 'https://fillmurray.com/200/300'
+              }
+              alt={movie.title}
+              className="img"
+            />
+          </Box>
+          <Tooltip disableTouchListener placement="top" title={movie.title}>
+            <Title className="title" variant="h5">
+              {movie.title}
+            </Title>
+          </Tooltip>
+          <Tooltip disableTouchListener title={`${movie.vote_average / 2} / 5`}>
+            <div>
+              <Rating readOnly value={movie.vote_average / 2} precision={0.1} />
+            </div>
+          </Tooltip>
+        </Links>
+      </Grow>
     </Grid>
   );
 };
